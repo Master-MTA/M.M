d0b8ca6a99012da25c0da4b3b09a5e2491476407
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-20 06:27:59 +0200

    Add files via upload

---
+++ elements/gridlist.lua
@@ -0,0 +1,256 @@
+
+function dxCreateGridList(x,y,width,height,baba,backcolor,bordercolor,linecolor)
+local backcolor=backcolor  or tocolor(0,0,0,255)
+local bordercolor=bordercolor  or tocolor(255,0,0,255)
+local linecolor=linecolor  or tocolor(255,255,255,255)
+local element=createElement('dxGridList')
+local baba=baba or false	
+
+	if element then
+	
+	if baba then
+		setElementParent(element,baba)
+		x=Elements[baba].x+x
+		y=Elements[baba].y+y+(Elements[baba].height*5/100)
+
+	end
+
+		Elements[element]={
+		type='gridList',
+		x=x,
+		y=y,
+		width=width,
+		height=height,
+		text=text,
+		backcolor=backcolor,
+		bordercolor=bordercolor,
+		linecolor=linecolor,
+		visible=false,
+		currentmove=false,
+		disabled=false,
+		moveable=false,
+		sizeable=false,
+		selecteditem=0,
+		selectedcolumn=0,
+		columns={},
+		rows={},
+		columncount=0,
+		rowcount=0,
+		visiblerows={},
+		unvisiblerowsdown={},
+		unvisiblerowsup={},
+		lastclickx=0,
+		lastclicky=0,
+		visiblerowcount=0,
+		scrolledrowsup=0,
+		myscroll=false,
+		}
+		table.insert(createdElements,element)
+
+		return element
+	end
+	return false
+end
+
+
+function dxGidListAddColumn(grid,title,colwidth,color)
+	
+	if Elements[grid] and Elements[grid].type=='gridList' then
+
+				local v=Elements[grid]
+				local color=color or tocolor(255,255,255,255)
+				local backcolor=backcolor or tocolor(40,115,178,255)
+				local currentnum=v.columncount	+ 1
+				local x,y,width,theheight,finalresult,num=0,0,0,0,0,v.columncount
+				
+				if v.columns[v.columncount] then
+					
+					while(num>=1)do
+						
+						local m= v.columns[num]
+						
+						finalresult=finalresult+m.width
+						
+						num=num-1
+					end
+					
+					finalresult=finalresult+(v.width*colwidth)
+					x=v.x+finalresult-(v.width*colwidth)
+					y=v.y
+					width=v.width*colwidth
+					theheight=v.height*5/100
+					else
+					x=v.x
+					y=v.y
+					width=v.width*colwidth
+					theheight=v.height*5/100
+					
+				end
+					
+		table.insert(Elements[grid].columns,{title=title,colwidth=colwidth,color=color,x=x,y=y,width=width,height=theheight,backcolor=backcolor})
+	
+		Elements[grid].columncount=Elements[grid].columncount+1
+		local element=createElement('dxGridListColumn',tostring(Elements[grid].columncount))
+		setElementParent(element,grid)
+		return Elements[grid].columncount
+	
+	end
+	
+return false
+
+end
+
+
+
+function dxGridListAddRow(grid)
+
+	if Elements[grid] and Elements[grid].type=='gridList' then
+		Elements[grid].rowcount=Elements[grid].rowcount+1
+
+	local element=createElement('dxGridListRow',tostring(Elements[grid].rowcount))
+		setElementParent(element,grid)
+		local myrow={}
+		
+			for k=1,Elements[grid].columncount do
+			
+				table.insert(myrow,{col=k})
+			
+			end
+			
+		table.insert(Elements[grid].rows,myrow)
+		
+		return Elements[grid].rowcount
+	
+	end
+	
+	return Elements[grid].rowcount
+	
+end
+
+
+function dxGridListGetColumnWidth(grid,col)
+
+	if Elements[grid] and Elements[grid].type=='gridList' then
+
+		return Elements[grid].columns[col].colwidth
+	
+	end
+	
+	return false
+
+end
+
+function dxGridListGetUnVisibleRowCount(grid)
+
+local result=0
+
+	if Elements[grid].visiblerowcount>=Elements[grid].rowcount then
+
+		result=Elements[grid].visiblerowcount-Elements[grid].rowcount
+		else
+		
+		result=Elements[grid].rowcount-Elements[grid].visiblerowcount
+	
+	end
+	
+	return result
+end
+
+
+function dxGridListSetItemText(grid,row,colnum,text,ty,color,backcolor,scale,font)
+
+	if Elements[grid] and Elements[grid].type=='gridList' then
+
+		local ty=ty or 'text'
+		
+		local font=font or 'default-bold'
+		
+		local scale=scale or 1.15
+		
+		local backcolor=backcolor or tocolor(40,115,178,70)
+
+			if ty=='text' then
+	
+				if Elements[grid].rows[row]  then
+				
+				local theta,num=getColumnTable(grid,colnum,row)
+				
+					if theta then
+					
+					local color=color or Elements[grid].columns[colnum].color
+					
+					local x,y,width,height=dxGridListGetColumnPosition(grid,colnum)
+					
+						if x then
+							local res=sumTheBeforeRows(grid,row,colnum)
+								
+								Elements[grid].rows[row][num]={col=colnum,text=text,positiontext='center',backcolor=backcolor,scale=scale,font=font,type=ty,x=x,y=y+res+5,width=width,height=dxGetFontHeight(scale,font)+5,color=color,iswork=true,myrow=row,anothercolor=backcolor,enterd=false,itemdata=false}
+									
+
+								
+									if Elements[grid].rows[row][num].y+Elements[grid].rows[row][num].height>Elements[grid].y+Elements[grid].height and not Elements[grid].myscroll then
+										
+										Elements[grid].myscroll=dxCreateScrollBar(Elements[grid].width-(Elements[grid].width*2/100),0,(Elements[grid].width*2/100),Elements[grid].height-(Elements[grid].height*5/100),grid)
+										
+									end
+									
+									
+									
+										local starty,endy=dxGetMaxAndMinRowNumber(grid)
+										
+										if starty then
+										
+											Elements[grid].visiblerows={}
+											Elements[grid].unvisiblerowsdown={}
+											Elements[grid].visiblerowcount=0
+											Elements[Elements[grid].myscroll].distanc=0
+											
+											for k=starty,endy do
+											Elements[grid].visiblerowcount=Elements[grid].visiblerowcount+1
+												table.insert(Elements[grid].visiblerows,Elements[grid].rows[k])
+											
+											end
+											
+											for k=endy+1,Elements[grid].rowcount do
+												table.insert(Elements[grid].unvisiblerowsdown,Elements[grid].rows[k])
+											end
+										
+										end
+										
+										if row~=oldrow then
+										
+											dxGridListchangeScrollBarOfSize(grid)
+											
+											oldrow=row
+											
+										end
+									
+										
+								for k,v in ipairs(Elements[grid].rows[row])do
+									
+									if not v.iswork then
+										
+										Elements[grid].rows[row][k]={col=v.col,itemdata=v.itemdata,text=text,positiontext='center',backcolor=backcolor,scale=scale,font=font,type=ty,x=x,y=y+res+5,width=width,height=dxGetFontHeight(scale,font)+5,color=color,iswork=false,myrow=row,anothercolor=backcolor,enterd=false}
+
+									
+									end
+								
+								end
+							
+							return true
+						
+						end
+						
+					end
+				
+				end
+				
+				
+			else
+	
+			end
+
+		return false
+	
+	end
+end
