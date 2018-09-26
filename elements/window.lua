ce10288b5f75dfe5526f43e82a1dedf26cce5189
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-26 08:09:50 +0200

    Update window.lua

--- elements/window.lua
+++ elements/window.lua
@@ -1,10 +1,10 @@
 
-function dxCreateWindow(x,y,width,height,text,backcolor,titlecolor,barcolor,btn)
-local backcolor=backcolor  or tocolor(0,0,0,125)
+function dxCreateWindow(x,y,width,height,text,backcolor,titlecolor,barcolor,bordercolor)
+local backcolor=backcolor  or tocolor(0,0,0,255)
 local titlecolor=titlecolor  or tocolor(255,255,255,255)
 local barcolor=barcolor  or tocolor(40,115,178,255)
+local bordercolor=bordercolor or tocolor(255,255,255,255)
 local element=createElement('dxWindow')
-local btn=btn or false
 	if element then
 		Elements[element]={
 		type='Window',
@@ -16,16 +16,40 @@ local btn=btn or false
 		backcolor=backcolor,
 		titlecolor=titlecolor,
 		barcolor=barcolor,
-		btn=btn,
 		visible=false,
 		currentmove=false,
 		disabled=false,
 		moveable=false,
 		sizeable=false,
-		lastclick=0,
+		lastclickx=0,
+		lastclicky=0,
 		}
 		table.insert(createdElements,element)
 		return element
 	end
+	return false
+end
+
+function dxWindowSetMovable(window,st)
+
+	if Elements[window] and Elements[window].type=='Window'	then
+	
+		Elements[window].moveable=st
+		
+		return true
+	
+	end
+
+	return false
+end
+
+function dxWindowIsMovable(window)
+
+	if Elements[window] and Elements[window].type=='Window'	then
+	
+		return Elements[window].moveable
+	
+	end
+
 	return false
 end
