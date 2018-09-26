c33c10242a04024eeaa6d9b8e86e2bcaab060bcb
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-26 08:09:04 +0200

    Update progressbar.lua

--- elements/progressbar.lua
+++ elements/progressbar.lua
@@ -1,12 +1,10 @@
 
-function dxCreateProgressBar(x,y,width,height,baba,text,backcolor,textcolor,progresscolor)
-local backcolor=backcolor  or tocolor(255,255,255,255)
-local textcolor=textcolor  or tocolor(40,115,178,255)
-local progresscolor=progresscolor  or tocolor(40,115,178,70)
-local text=text or false
-local element=createElement('dxProgressBar')
+function dxCreateRadioButton(x,y,width,height,text,baba,backcolor,textcolor)
+local backcolor=backcolor  or tocolor(40,115,178,255)
+local textcolor=textcolor  or tocolor(255,255,255,255)
+local element=createElement('dxRadioButton')
+local text=text or ''
 local baba=baba or false	
-local othertext=text
 	if element then
 	if baba then
 		setElementParent(element,baba)
@@ -14,25 +12,21 @@ local othertext=text
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
 
 	end
-
-			
 		Elements[element]={
-		type='progressBar',
+		type='radioButton',
 		x=x,
 		y=y,
 		width=width,
 		height=height,
 		text=text,
-		othertext=othertext,
 		baba=baba,
 		backcolor=backcolor,
 		textcolor=textcolor,
 		positiontext='center',
-		progresscolor=progresscolor,
 		visible=false,
-		distanc=0,
+		isenabled=false,
 		disabled=false,
-		lastclick=0,
+		isselected=false,
 		}
 		table.insert(createdElements,element)
 		return element
@@ -41,44 +35,20 @@ local othertext=text
 end
 
 
-function dxProgressBarSetProgress(element,pr)
 
-if pr<0 or pr > 100 then return false end
+function dxRadioButtonGetSelected(check)
+	if Elements[check] and Elements[check].type=='radioButton' then
+		return Elements[check].isselected
+	end
+return false
+end
 
-	if Elements[element] and Elements[element].type=='progressBar' then
-	
-		Elements[element].distanc=Elements[element].width*pr/100
-		
+
+function dxRadioButtonSetSelected(check,st)
+	if Elements[check] and Elements[check].type=='radioButton' then
+		 Elements[check].isselected=st
 		return true
-	
 	end
-
 return false
 end
 
-function dxProgressBarSetText(element,text)
-
-if type(text)~='string' then return false end
-
-	if Elements[element] and Elements[element].type=='progressBar' then
-	
-		Elements[element].text=text
-		
-		return true
-	
-	end
-
-return false
-end
-
-function dxProgressBarGetProgress(element)
-
-
-	if Elements[element] and Elements[element].type=='progressBar' then
-	
-			return (Elements[element].distanc/Elements[element].width)*100
-			
-	end
-
-return false
-end
