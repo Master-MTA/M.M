8ea935e74d4fb5e0438379698ceb3791e00015b1
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-28 10:25:34 +0200

    Update editbox.lua

--- elements/editbox.lua
+++ elements/editbox.lua
@@ -1,8 +1,8 @@
 
 function dxCreateEdit(x,y,width,height,text,baba,backcolor,textcolor,bordercolor,anothercolor)
-local backcolor=backcolor  or tocolor(40,115,178,255)
-local textcolor=textcolor  or tocolor(255,255,255,255)
-local bordercolor=bordercolor  or tocolor(255,255,255,255)
+local backcolor=backcolor  or tocolor(255,255,255,255)
+local textcolor=textcolor  or tocolor(0,0,0,255)
+local bordercolor=bordercolor  or tocolor(40,115,178,255)
 local anothercolor=anothercolor or tocolor(0,0,0,255)
 local element=createElement('dxEdit')
 local baba=baba or false	
@@ -13,7 +13,22 @@ local othertext=text
 		setElementParent(element,baba)
 		x=Elements[baba].x+x
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
-
+	if x+width>Elements[baba].x+Elements[baba].width then
+	
+		local res=(Elements[baba].width)-((x-Elements[baba].x))
+		
+		width=res
+	
+	end
+	
+	if y+height>Elements[baba].y+Elements[baba].height then
+	
+		local res=(Elements[baba].height-(Elements[baba].height*5/100))-((y)-(Elements[baba].y+(Elements[baba].height*5/100)))
+		
+		height=res
+	
+	end
+	
 	end
 		local edit=guiCreateEdit(x,y,width,height,text,false)
 			guiSetVisible(edit,false)
@@ -50,6 +65,7 @@ local othertext=text
 		lastclick=0,
 		}
 		table.insert(createdElements,element)
+							
 		return element
 	end
 	return false
