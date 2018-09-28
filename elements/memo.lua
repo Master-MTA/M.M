b2e4b1041cb751eadde27894189d4f55c791a660
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-28 10:28:48 +0200

    Update memo.lua

--- elements/memo.lua
+++ elements/memo.lua
@@ -1,7 +1,7 @@
 
 function dxCreateMemo(x,y,width,height,text,baba,backcolor,textcolor,bordercolor)
-local backcolor=backcolor  or tocolor(40,115,178,255)
-local textcolor=textcolor  or tocolor(255,255,255,255)
+local backcolor=backcolor  or tocolor(255,255,255,255)
+local textcolor=textcolor  or tocolor(0,0,0,255)
 local bordercolor=bordercolor  or tocolor(40,115,178,255)
 local element=createElement('dxMemo')
 local baba=baba or false	
@@ -10,6 +10,21 @@ local baba=baba or false
 		setElementParent(element,baba)
 		x=Elements[baba].x+x
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
+			if x+width>Elements[baba].x+Elements[baba].width then
+	
+		local res=(Elements[baba].width)-((x-Elements[baba].x))
+		
+		width=res
+	
+	end
+	
+	if y+height>Elements[baba].y+Elements[baba].height then
+	
+		local res=(Elements[baba].height-(Elements[baba].height*5/100))-((y)-(Elements[baba].y+(Elements[baba].height*5/100)))
+		
+		height=res
+	
+	end
 
 	end
 		local memo=guiCreateMemo(x,y,width,height,text,false)
@@ -38,6 +53,7 @@ local baba=baba or false
 		lastclick=0,
 		}
 		table.insert(createdElements,element)
+
 		return element
 	end
 	return false
