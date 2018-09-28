56f172ee0c307b0c06556af53db66dc883ef9377
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-28 10:25:10 +0200

    Update checkbox.lua

--- elements/checkbox.lua
+++ elements/checkbox.lua
@@ -1,6 +1,7 @@
 
-function dxCreateCheckBox(x,y,width,height,text,baba,backcolor,textcolor,selectcolor)
-local backcolor=backcolor  or tocolor(187,187,187,125)
+function dxCreateCheckBox(x,y,width,height,text,baba,backcolor,backcolor2,textcolor,selectcolor)
+local backcolor=backcolor  or tocolor(169,169,169,255)
+local backcolor2=backcolor2  or tocolor(0,0,0,255)
 local textcolor=textcolor  or tocolor(255,255,255,255)
 local selectcolor=selectcolor  or tocolor(40,115,178,255)
 local element=createElement('dxCheckBox')
@@ -10,6 +11,21 @@ local baba=baba or false
 		setElementParent(element,baba)
 		x=Elements[baba].x+x
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
+			if x+width>Elements[baba].x+Elements[baba].width then
+	
+		local res=(Elements[baba].width)-((x-Elements[baba].x))
+		
+		width=res
+	
+	end
+	
+	if y+height>Elements[baba].y+Elements[baba].height then
+	
+		local res=(Elements[baba].height-(Elements[baba].height*5/100))-((y)-(Elements[baba].y+(Elements[baba].height*5/100)))
+		
+		height=res
+	
+	end
 
 	end
 		Elements[element]={
@@ -21,6 +37,7 @@ local baba=baba or false
 		text=text,
 		baba=baba,
 		backcolor=backcolor,
+		backcolor2=backcolor2,
 		textcolor=textcolor,
 		position='center',
 		distanc=0,
