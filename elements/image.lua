8a8ea93267e8d1b4e21bfdcd28bd814c007293e3
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-09-28 10:27:00 +0200

    Update image.lua

--- elements/image.lua
+++ elements/image.lua
@@ -8,6 +8,21 @@ local baba=baba or false
 		setElementParent(element,baba)
 		x=Elements[baba].x+x
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
+			if x+width>Elements[baba].x+Elements[baba].width then
+	
+		local res=(Elements[baba].width)-((x-Elements[baba].x))
+		
+		width=res
+	
+	end
+	
+	if y+height>Elements[baba].y+Elements[baba].height then
+	
+		local res=(Elements[baba].height-(Elements[baba].height*5/100))-((y)-(Elements[baba].y+(Elements[baba].height*5/100)))
+		
+		height=res
+	
+	end
 
 	end
 
@@ -25,6 +40,7 @@ local baba=baba or false
 		lastclick=0,
 		}
 		table.insert(createdElements,element)
+
 		return element
 	end
 	return false
