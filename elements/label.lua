5e5527783ec390e946aa6be0f5142651bcfad95c
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-10-05 00:33:18 +0200

    Update label.lua

--- elements/label.lua
+++ elements/label.lua
@@ -4,12 +4,13 @@ local textcolor=textcolor  or tocolor(255,255,255,255)
 local element=createElement('dxLabel')
 local baba=baba or false	
 	if element then
-	if baba then
+	if baba  then
 		setElementParent(element,baba)
+
+	if  Elements[baba].type~='Tab' then
 		x=Elements[baba].x+x
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
-		
-			if x+width>Elements[baba].x+Elements[baba].width then
+		if x+width>Elements[baba].x+Elements[baba].width then
 	
 		local res=(Elements[baba].width)-((x-Elements[baba].x))
 		
@@ -24,7 +25,25 @@ local baba=baba or false
 		height=res
 	
 	end
-
+		else
+		x=Elements[getElementParent(baba)].x+x
+		y=Elements[getElementParent(baba)].y+y+(Elements[getElementParent(baba)].height*5/100)
+		if x+width>Elements[getElementParent(baba)].x+Elements[getElementParent(baba)].width then
+	
+		local res=(Elements[getElementParent(baba)].width)-((x-Elements[getElementParent(baba)].x))
+		
+		width=res
+	
+	end
+	
+	if y+height>Elements[getElementParent(baba)].y+Elements[getElementParent(baba)].height then
+	
+		local res=(Elements[getElementParent(baba)].height-(Elements[getElementParent(baba)].height*5/100))-((y)-(Elements[getElementParent(baba)].y+(Elements[getElementParent(baba)].height*5/100)))
+		
+		height=res
+	
+	end
+	end
 	end
 		Elements[element]={
 		type='Label',
@@ -42,7 +61,11 @@ local baba=baba or false
 		lastclick=0,
 		}
 		table.insert(createdElements,element)
+			if sourceResource then
+			if not resElements[getResourceName(sourceResource)] then resElements[getResourceName(sourceResource)]={} end
 
+			table.insert(resElements[getResourceName(sourceResource)],element)
+			end
 		return element
 	end
 	return false
