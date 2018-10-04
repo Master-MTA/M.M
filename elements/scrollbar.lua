10c989fb305b15b6549cceba84de0ad89913f15f
Master-MTA <43430031+Master-MTA@users.noreply.github.com>
2018-10-05 01:19:29 +0200

    Update scrollbar.lua

--- elements/scrollbar.lua
+++ elements/scrollbar.lua
@@ -6,19 +6,13 @@ local backcolor=backcolor or tocolor(255,255,255,255)
 local movecolor=movecolor or tocolor(40,115,178,255)
 local element=createElement('dxScrollBar')
 	if element then
-	if baba then
+	if baba  then
 		setElementParent(element,baba)
+
+	if  Elements[baba].type~='Tab' then
 		x=Elements[baba].x+x
-		if Elements[baba].type=='gridList' or Elements[baba].type=='Window' then
 		y=Elements[baba].y+y+(Elements[baba].height*5/100)
-		else
-		y=Elements[baba].y+y+(Elements[baba].height)
-		end
-		
-		if Elements[baba].type=='gridList' then
-			
-		end
-			if x+width>Elements[baba].x+Elements[baba].width then
+		if x+width>Elements[baba].x+Elements[baba].width then
 	
 		local res=(Elements[baba].width)-((x-Elements[baba].x))
 		
@@ -32,6 +26,25 @@ local element=createElement('dxScrollBar')
 		
 		height=res
 	
+	end
+		else
+		x=Elements[getElementParent(baba)].x+x
+		y=Elements[getElementParent(baba)].y+y+(Elements[getElementParent(baba)].height*5/100)
+		if x+width>Elements[getElementParent(baba)].x+Elements[getElementParent(baba)].width then
+	
+		local res=(Elements[getElementParent(baba)].width)-((x-Elements[getElementParent(baba)].x))
+		
+		width=res
+	
+	end
+	
+	if y+height>Elements[getElementParent(baba)].y+Elements[getElementParent(baba)].height then
+	
+		local res=(Elements[getElementParent(baba)].height-(Elements[getElementParent(baba)].height*5/100))-((y)-(Elements[getElementParent(baba)].y+(Elements[getElementParent(baba)].height*5/100)))
+		
+		height=res
+	
+	end
 	end
 	end
 		Elements[element]={
@@ -54,7 +67,11 @@ local element=createElement('dxScrollBar')
 		lastclick=0,
 		}
 		table.insert(createdElements,element)
+			if sourceResource then
+						if not resElements[getResourceName(sourceResource)] then resElements[getResourceName(sourceResource)]={} end
 
+			table.insert(resElements[getResourceName(sourceResource)],element)
+			end
 		return element
 	end
 	return false
